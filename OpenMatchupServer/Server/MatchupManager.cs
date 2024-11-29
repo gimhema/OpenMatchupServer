b60cf8dcc91c38ecdaeaff9e3528be73cdbf0fb6
kimmo <hemakim0215@naver.com>
2024-11-29 09:04:17 +0000

    .

--- OpenMatchupServer/Server/MatchupManager.cs
+++ OpenMatchupServer/Server/MatchupManager.cs
@@ -10,9 +10,20 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace OpenMatchupServer.Server
 {
+    public struct Team
+    {
+        List<GamePlayer> members;
+    }
+
+    public class GameMatch
+    {
+        List<Team> competitions;
+    }
 
     public class MatchupManager
     {
+        public Dictionary<int, MatchMakingInfo> matchContainer = new Dictionary<int, MatchMakingInfo>();
+
         private static MatchupManager _instance;
 
         private static readonly object _lock = new object();
