20c0c9e666e56a8f85f764ae1a62e899e8c1c9fc
kimmo <hemakim0215@naver.com>
2024-12-08 11:22:49 +0900

    .

--- OpenMatchupServer/Data/Packets.cs
+++ OpenMatchupServer/Data/Packets.cs
@@ -4,6 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 using Newtonsoft.Json;
 using System.Threading;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 
@@ -12,7 +13,7 @@ namespace OpenMatchupServer.Packets
 {
     abstract class Packet
     {
-
+        public int funtionId {get; set;} = 0;
         // JSON -> Packet
         public abstract void Deserialize(string data);
 
@@ -35,7 +36,34 @@ namespace OpenMatchupServer.Packets
         // data -> message class
         public override void Deserialize(string data)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var result = JObject.Parse(data);
+
+                if (result["id"] != null &&
+                    result["pID"] != null &&
+                    result["userName"] != null &&
+                    result["ratingPoint"] != null)
+                {
+                    funtionId = int.Parse(result["id"].ToString());
+                    pID = int.Parse(result["pID"].ToString());
+                    name = result["userName"].ToString();
+                    ratingPoint = int.Parse(result["ratingPoint"].ToString());
+                }
+                else
+                {
+                    throw new Exception("The 'id' field is missing in the JSON data.");
+                }
+            }
+            catch (JsonReaderException ex)
+            {
+                Console.WriteLine($"Invalid JSON format: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
         }
 
         // message class -> data
